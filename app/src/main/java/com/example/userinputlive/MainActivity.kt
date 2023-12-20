9baa6e6bf834e7a146fbb90141528e3cc5cf6989
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-12-20 20:08:34 +0100

    logik button, der den input aus textedit löscht

--- app/src/main/java/com/example/userinputlive/MainActivity.kt
+++ app/src/main/java/com/example/userinputlive/MainActivity.kt
@@ -1,17 +1,14 @@
 package com.example.userinputlive
 
 import android.os.Bundle
+import android.text.Editable
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.RadioButton
+import android.widget.TextView
 import com.example.userinputlive.databinding.ActivityMainBinding
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
@@ -25,16 +22,31 @@ class MainActivity : AppCompatActivity() {
     private lateinit var thirdRadio: RadioButton
     private lateinit var button: Button
     private lateinit var fab: FloatingActionButton
+    private lateinit var input: String
+    private lateinit var outputTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fab = findViewById(R.id.fab)
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "FAB getappt!",Snackbar.LENGTH_LONG)
+        // Input aus TextEdit rausschälen
+        var input = findViewById<EditText>(R.id.editText).text
+
+
+        // Logik FAB
+        fab = findViewById<FloatingActionButton>(R.id.fab)
+        fab.setOnClickListener {
+            Snackbar.make(it, "FAB getappt!",Snackbar.LENGTH_LONG)
                 .show()
         }
+
+        // Logik Button
+        button = findViewById(R.id.button)
+        button.setOnClickListener{
+            input.clear()
+        }
+
+        // Logik TextView, die den in TextEdit eingegebenen
 
 
 
