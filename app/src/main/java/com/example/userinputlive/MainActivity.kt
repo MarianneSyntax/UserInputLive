aab127fee18f003c7bf8eddc8dcafe70b228b0fd
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-12-20 18:14:38 +0100

    elemente in der activity main als lateinit variablen angelegt, FAB funktionalität mit dem setOnClickListener gegeben = eine Snackbar erscheint

--- app/src/main/java/com/example/userinputlive/MainActivity.kt
+++ app/src/main/java/com/example/userinputlive/MainActivity.kt
@@ -9,16 +9,37 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.RadioButton
 import com.example.userinputlive.databinding.ActivityMainBinding
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var firstRadio: RadioButton
+    private lateinit var sndRadio: RadioButton
+    private lateinit var thirdRadio: RadioButton
+    private lateinit var button: Button
+    private lateinit var fab: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fab = findViewById(R.id.fab)
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "FAB getappt!",Snackbar.LENGTH_LONG)
+                .show()
+        }
+
+
+
+
+
 
     }
 
